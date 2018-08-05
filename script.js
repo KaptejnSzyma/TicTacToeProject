82c4b44427f6e155957d609192dddeed13070e75
Szymek <szymon.rzadca@gmail.com>
2018-08-05 12:15:16 +0200

    game logic completed

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 var restart = document.querySelector('#restart')
-var head = document.querySelector('#header')
+var squares = document.querySelectorAll('td')
 
-restart.addEventListener('click', function(){
-  head.textContent = 'Shit'
-})
+function clearBoard() {
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].textContent = '';
+  }
+}
+
+restart.addEventListener('click', clearBoard);
+
+function changeMarker(){
+  if(this.textContent === ''){
+    this.textContent = 'X';
+  }else if (this.textContent === 'X') {
+    this.textContent = 'O';
+  }else {
+    this.textContent = '';
+  }
+}
+
+for (var i = 0; i < squares.length; i++) {
+  squares[i].addEventListener('click', changeMarker)
+}
